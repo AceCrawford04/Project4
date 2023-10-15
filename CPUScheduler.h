4c1b7b1beac87d6f9a91bf087b75f0f98f6d8e79
AceCrawford04 <132942399+AceCrawford04@users.noreply.github.com>
2023-10-15 11:18:45 -0400

    Add files via upload

---
+++ CPUScheduler.h
@@ -0,0 +1,17 @@
+//File: CPUscheduler.h
+//Ace Crawford, Cameron Herine, and Theresa Redford 
+//Fall 2023, Transy
+//Operating Systems
+//Interface: (continue description)
+//(what works, what does not)
+
+#ifndef CPUSCHEDULER_H
+#define CPUSCHEDULER_H
+
+//#include <studio.h>
+#include <io.stream>
+#include <string>
+
+using namespace std;
+
+#endif
