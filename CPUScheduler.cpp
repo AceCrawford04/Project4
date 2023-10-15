64f12373e491bd84c45bd51b45f4009b1265baa6
AceCrawford04 <132942399+AceCrawford04@users.noreply.github.com>
2023-10-15 11:18:26 -0400

    Add files via upload

---
+++ CPUScheduler.cpp
@@ -0,0 +1,17 @@
+//File: CPUscheduler.cpp
+//Ace Crawford, Cameron Herine, and Theresa Redford 
+//Fall 2023, Transy
+//Operating Systems
+//This is the driver file for our CPU scheduler, (continue description)
+//(what works, what does not)
+
+#include "CPUScheduler.h"
+
+
+int main (int argc, char** argv){
+
+if(argc == 0){
+	cout << "No arguments were passed" << endl;
+	}
+
+}
