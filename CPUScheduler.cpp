de906ec77730afb01992df6278038f1d68467616
AceCrawford04 <132942399+AceCrawford04@users.noreply.github.com>
2023-10-17 16:16:08 -0400

    Update CPUScheduler.cpp
    I added the flags for type and preemtive, as well as took in a file and checked if it was open

--- CPUScheduler.cpp
+++ CPUScheduler.cpp
@@ -10,8 +10,77 @@
 
 int main (int argc, char** argv){
 
-if(argc == 0){
+//we can delete these, but I find them helpful for testing
+cout << argc << endl;
+
+for (int i=0; i < argc; i++){
+			cout << argv[i] << endl;
+		
+		}
+	
+cout << endl;
+
+ifstream inFile;
+
+bool FCFS, SJF, Priority, RR;
+
+bool preemtive;
+
+FCFS = true;
+
+preemtive = false;
+
+if(argc == 1){
 	cout << "No arguments were passed" << endl;
 	}
+
+//I would love if we could figure out how to not have to make these strings
+string stringSJF = "--typeSJF";
+string stringRR = "--typeRR";
+string stringPriority = "--typePriority";
+string stringPreemptive = "--preemptive";
+for (int i=0; i < argc; i++){
+	//tests for type
+		if(argv[i] == stringSJF)
+{
+	FCFS = false;
+	SJF = true;
+	cout << "type is: --typeSJF" << endl;
+
+}
+	else if(argv[i] == stringRR)
+{
+	FCFS = false;
+	RR = true;
+	cout << "type is: --typeRR" << endl;
+
+}
+	else if(argv[i] == stringPriority)
+{
+	FCFS = false;
+	Priority = true;
+	cout << "type is: --typePriority" << endl;
+
+}
+
+//tests for preemptive
+	if(argv[i] == stringPreemptive){
+		preemtive = false;
+		cout << "preemtive" << endl;
+	}
+}
+
+	
+
+//change file name to variable
+inFile.open ("sched.in");
+
+//we can take some/all of this out, but I find it helpful for testing purposes
+if (!inFile.is_open()) {
+	cout << "Error: sched.in could not be opened" << endl;
+}
+else {
+	cout << "File sucessfully opened!" << endl;
+}
 
 }
