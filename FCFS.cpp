d5f29085ab054673651d8ec6ae2d76807fedf494
CameronH25 <110263516+CameronH25@users.noreply.github.com>
2023-10-16 20:49:23 -0400

    Hello FCFS!
    Adding the basic files for FCFS. 
    Please read the comment in FCFS.cpp and let me know what you all think!

---
+++ FCFS.cpp
@@ -0,0 +1,11 @@
+// implementation file for FCFS. 
+
+// I was thinking we had a file like this for each of the algo's 
+// then perhaps we could have a file for the Queues -- ready, waiting, etc.
+// and then have a file for the final caluclations? It depends on how you guys
+// want to do it. We can easily create/delete these branches on git, if you all want.
+
+// we could also have another file for receiving the arguments and whatnot,
+// and calls whatever it needs to perform the calculations and whatnot for awt, 
+// then returns the awt to main, to keep main pretty. 
+
