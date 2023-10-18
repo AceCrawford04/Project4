88eb1b1195f1a3e09ee353ae74cf9591d1297b0f
CameronH25 <110263516+CameronH25@users.noreply.github.com>
2023-10-18 02:02:16 -0400

    FCFS  -  UPDATE TO .cpp AND .h
    Logic for FCFS and its header file update here.

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,11 +1,57 @@
 // implementation file for FCFS. 
 
-// I was thinking we had a file like this for each of the algo's 
-// then perhaps we could have a file for the Queues -- ready, waiting, etc.
-// and then have a file for the final caluclations? It depends on how you guys
-// want to do it. We can easily create/delete these branches on git, if you all want.
+#include "FCFS.h"
+#include <iostream>
+#include <fstream>
 
-// we could also have another file for receiving the arguments and whatnot,
-// and calls whatever it needs to perform the calculations and whatnot for awt, 
-// then returns the awt to main, to keep main pretty. 
+using namespace std; 
+
+FCFS::FCFS() {
+    // Any initialization for FCFS can go here
+}
+
+void FCFS::loadProcessesFromFile(const string& fileName) {
+    ifstream inFile(fileName);
+
+    if (!inFile.is_open()) {
+        cout << "Error: Could not open file " << fileName << endl;
+        return;
+    }
+
+    string id;
+    int arrival, burst, priority;
+    while (inFile >> id >> arrival >> burst >> priority) {
+        Process newProcess(id, arrival, burst, priority);
+        readyQueue.push(newProcess);
+    }
+}
+
+void FCFS::execute(bool verbose) {
+    int currentTime = 0;  // Keep track of current time
+    double totalWaitTime = 0;  // Accumulator for total waiting time
+
+    while (!readyQueue.empty()) {
+        Process current = readyQueue.front();
+        readyQueue.pop();
+
+        // Calculate wait time for this process
+        int waitTime = currentTime - current.Arrival_Time;
+        totalWaitTime += waitTime;
+
+        // Verbose output
+        if (verbose) {
+            cout << "Time " << currentTime << ": Processing " << current.ID << endl;
+        }
+
+        // Simulate processing...
+        currentTime += current.Total_CPU_Burst;  // Add burst time to current time
+    }
+
+    averageWaitTime = totalWaitTime / currentTime;
+    cout << "Average Wait Time: " << averageWaitTime << endl;
+}
+
+
+
+
 
--- FCFS.h
+++ FCFS.h
@@ -1,1 +1,22 @@
 // header file for FCFS.
+
+#ifndef FCFS_H
+#define FCFS_H
+
+#include "procManagement.h"
+#include <vector>
+
+class FCFS {
+public:
+    FCFS();  // Constructor
+    void loadProcessesFromFile(const string& fileName);  // Load processes from a file
+    void execute(bool verbose = false);  // Executes the FCFS scheduling
+
+private:
+    queue<Process> readyQueue;  // Queue of ready processes
+    double averageWaitTime = 0;  // To calculate average wait time
+
+    // other methods can go here
+};
+
+#endif // FCFS_H
