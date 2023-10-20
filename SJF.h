5a1fa8951500f633a639f82fff1e4c89c63e4be1
AceCrawford04 <132942399+AceCrawford04@users.noreply.github.com>
2023-10-20 16:29:00 -0400

    Update SJF.h

--- SJF.h
+++ SJF.h
@@ -10,13 +10,15 @@ class SJF {
 public:
     SJF();  // Constructor
     int loadProcessesFromFile(const string& fileName);  // Load processes from a file
-    void execute(bool verbose = false, int processCount = 1);  // Executes the SJF scheduling
-
+    void execute(bool verbose = false, int processCount = 0);  // Executes the SJF scheduling
+    void putQueue(const string& fileName, int count);
+   // void sortQueue(int&, int count);
 private:
+
     queue<Process> readyQueue;  // Queue of ready processes
     double averageWaitTime = 0;  // To calculate average wait time
-
-    // other methods can go here
 };
 
 #endif // SJF_H
+
+//./project4 --type SJF --verbose
