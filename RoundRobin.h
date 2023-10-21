82ef8889bd913cc7ed96e272572df8fc6135c64a
TcRedford <113555206+TcRedford@users.noreply.github.com>
2023-10-21 03:44:33 +0200

    Update RoundRobin.h

--- RoundRobin.h
+++ RoundRobin.h
@@ -1,17 +1,16 @@
 //File: RoundRobin.h
 //Ace Crawford, Cameron Herine, and Theresa Redford 
+//File: RoundRobin.h
+//Ace Crawford, Cameron Herine, and Theresa Redford 
 //Fall 2023, Transy
 //Operating Systems
 //Interface for RoundRobin
-
-
 
 #ifndef ROUNDROBIN_H
 #define ROUNDROBIN_H
 
 #include <iostream>
 #include <string>
-#include <queue>
 
 using namespace std;
 
@@ -19,27 +18,21 @@ Class RoundRobin{
 
 public:
 
-	RoundRobin(void);
-	~RoundRobin(void);
-
-	// public member function
+RoundRobin(void);
+~RoundRobin(void);
+	
 // constructor for the Process struct
-   RoundRobin::Process(const string& n, int p, int rt) {
-    name = n;
-    priority = p;
-    remainingTime = rt;
+   RoundRobin::Process(const string& iProcess, int pProcess, int rtProcess) {
+    id = iProcess;
+    priority = pProcess;
+    remainingTime = rtProcess;
     waitTime = 0;
 }
 
-
-// determinePriorityProcesses function goes here
-// loadProcessesFromFile function goes here
-// print goes here
+int RoundRobin::loadProcessesFromFile(const string& fileName);
+void RoundRobin :: print(void);
 
 private:
-
-	// private members
-
 };
 
 #endif
